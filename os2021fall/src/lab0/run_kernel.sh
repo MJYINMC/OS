e1862004113ea5f4c83a5d38b6432c21d0b7697f
MJYINMC <1397102156@qq.com>
2021-09-15 03:50:37 +0800

    lab0

---
+++ os2021fall/src/lab0/run_kernel.sh
@@ -0,0 +1,1 @@
+qemu-system-riscv64 -nographic -machine virt -kernel ../../../linux-5.14.3/arch/riscv/boot/Image -device virtio-blk-device,drive=hd0 -append "root=/dev/vda ro console=ttyS0" -bios default -drive file=rootfs.img,format=raw,id=hd0 -S -s
